34a8657db0e38526f8435771e8bc7912a60fcc41
pugachev67 <pugachev6700@mail.ru>
2021-07-27 17:48:52 +0400

    Work

---
+++ work/src/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/Weather";
+
+const API_KEY = "abe9c961db0a7bed9c2f9e2eee7bf789"
+
+class App extends React.Component {
+
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
+ gettingWeather = async (event) => {
+   event.preventDefault();
+  var city = event.target.elements.city.value;
+
+if(city){
+  const api_url = await
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+  const data = await api_url.json();
+
+  var sunset = data.sys.sunset;
+  var date = new Date();
+  date.setTime(sunset);
+  var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+   this.setState({
+     temp: data.main.temp,
+     city: data.name,
+     country: data.sys.country,
+     pressure: data.main.pressure,
+     sunset: sunset_date,
+     error: undefined
+   });
+ } else{
+   this.setState({
+     temp: undefined,
+     city: undefined,
+     country: undefined,
+     pressure: undefined,
+     sunset: undefined,
+     error: "Введите название города"
+   });
+ }
+ }
+
+  render() {
+    return (
+      <div className="wrapper">
+      <div className="row">
+      <div className="col-sm-5 info">
+      <Info />
+      </div>
+      <div className="col-sm-7 form">
+      <Form weatherMethood={this.gettingWeather} />
+      <Weather
+temp={this.state.temp}
+city={this.state.city}
+country={this.state.country}
+pressure={this.state.pressure}
+sunset={this.state.sunset}
+error={this.state.error}
+      />
+      </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
